85c13fac0e6839ff7aa721e52a03a359b1994b83
CarlosM-08 <carlos_murillo1@unitec.edu>
2026-08-24 18:56:52 -0600

    Iniciar entrada de teclado

--- Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
+++ Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
@@ -14,7 +14,9 @@ public class Proyecto_SistemaAlquilerVehiculos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // INICIO DEL PROYECTO
+        
+    
     }
     
 }
