c59eee5639f5e6bb4cf758d40fb65375ca7a7562
CarlosM-08 <carlos_murillo1@unitec.edu>
2026-08-30 09:36:13 -0600

    Actualizacion - Seccion ingreso de clientes y consulta de clientes

--- Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
+++ Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
@@ -18,109 +18,146 @@ public class Proyecto_SistemaAlquilerVehiculos {
     public static void main(String[] args) {
         // INICIO DEL PROYECTO
         Scanner input = new Scanner(System.in);
-        
+
         //Declaracion de variables
         String[] cliente = new String[10];
         int[] edad = new int[10];
         String[] identidad = new String[10];
-        String[] licencia = new String [10];   
+        String[] licencia = new String[10];
         int respuestaTipoVehiculo = 0;
         String respuestaRegistro = "";
         int eleccion = 0;
-        
-        
-        //MENU PRINCIPAL        
-        System.out.println("BIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
-        System.out.println("-----------------------------------------------");
-        System.out.println();
-        System.out.println("""
+        int cantidadClientes = 0;
+        int capacidadMaxClientes = 10;
+        String valorBuscado = "0";
+
+        do {
+
+            //MENU PRINCIPAL        
+            System.out.println("BIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
+            System.out.println("-----------------------------------------------");
+            System.out.println();
+            System.out.println("""
                            Que gestion desea realizar?
                            1. Registrar Cliente
-                           2. Registrar Alquiler
-                           3. Registrar Devolucion
-                           4. Consulta de Vehiculos
-                           5. Mostrar resumen general
-                           6. Salir
+                           2. Consultar Cliente
+                           3. Registrar Alquiler
+                           4. Registrar Devolucion
+                           5. Consulta de Vehiculos
+                           6. Mostrar resumen general
+                           7. Salir
                            """);
-        
-        System.out.print("Eleccion: ");
-        eleccion = input.nextInt();
-        System.out.println();
 
-        switch (eleccion) {
-            case 1:
-                //SECCION REGISTRO DE CLIENTE
-                System.out.println("== REGISTRO DE CLIENTES==");
-                System.out.println("");
+            System.out.print("Eleccion: ");
+            eleccion = input.nextInt();
+            input.nextLine();
+            System.out.println();
 
-                for (int i = 0; i < 10; i++) {
-                    System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
-                    input.nextLine();
-                    respuestaRegistro = input.nextLine().toUpperCase();
-                    if (respuestaRegistro.equals("SI")) {
-                        System.out.print("Ingrese el nombre del cliente: ");
-                        cliente[i] = input.nextLine();
-                        System.out.print("Ingrese la edad del cliente: ");
-                        edad[i] = input.nextInt();
-                        System.out.print("Ingrese el numero de identidad del cliente: ");
-                        input.nextLine();
-                        identidad[i] = input.nextLine();
-                        System.out.print("Ingrese el numero de licencia del cliente: ");
-                        licencia[i] = input.nextLine();
+            switch (eleccion) {
+                case 1:
+                    //SECCION REGISTRO DE CLIENTE
+                    System.out.println("== REGISTRO DE CLIENTES==");
+                    System.out.println("");
+                    for (int i = cantidadClientes; i < capacidadMaxClientes; i++) {
+                        System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
 
-                        System.out.println();                        
-                        System.out.println("== RESUMEN DE REGISTRO ==");
-                        System.out.println("-------------------------");
-                        System.out.println("Nombre del cliente: " + cliente[i]);
-                        System.out.println("edad del cliente: " + edad[i]);
-                        System.out.println("identidad del cliente: " + identidad[i]);
-                        System.out.println("No de licencia del cliente: " + licencia[i]);
-                        System.out.println();
+                        respuestaRegistro = input.nextLine().toUpperCase();
+                        if (respuestaRegistro.equals("SI")) {
+                            System.out.print("Ingrese el nombre del cliente: ");
+                            cliente[i] = input.nextLine();
+                            System.out.print("Ingrese la edad del cliente: ");
+                            edad[i] = input.nextInt();
+                            System.out.print("Ingrese el numero de identidad del cliente: ");
+                            input.nextLine();
+                            identidad[i] = input.nextLine();
+                            System.out.print("Ingrese el numero de licencia del cliente: ");
+                            licencia[i] = input.nextLine();
+
+                            System.out.println();
+                            System.out.println("== RESUMEN DE REGISTRO ==");
+                            System.out.println("-------------------------");
+                            System.out.println("Nombre del cliente: " + cliente[i]);
+                            System.out.println("edad del cliente: " + edad[i]);
+                            System.out.println("identidad del cliente: " + identidad[i]);
+                            System.out.println("No de licencia del cliente: " + licencia[i]);
+                            System.out.println();
+
+                            cantidadClientes++;
+
+                        } else {
+                            break;
+                        }//Fin if/else
+                    }//Fin for 
+
+                    break;
+                case 2:
+                    //SECCION CONSULTA DE CLIENTE
+                    System.out.println("== CONSULTA DE CLIENTES ==");
+                    System.out.println(""); 
+                    
+                    if (cantidadClientes == 0) {
+                        System.out.println("NO hay clientes registrados\n");
                     } else {
-                        break;
-                    }//Fin if/else
-                }//Fin for 
+                        System.out.print("Ingrese el numero de identidad del cliente: ");
+                        valorBuscado = input.nextLine();
+                        for (int i = 0; i < cantidadClientes; i++) {
+                            if (cantidadClientes > 0) {
+                                if (valorBuscado.equals(identidad[i])) {
+                                    System.out.println("Nombre del cliente: " + cliente[i]);
+                                    System.out.println("edad del cliente: " + edad[i]);
+                                    System.out.println("identidad del cliente: " + identidad[i]);
+                                    System.out.println("licencia del cliente: " + licencia[i]);
+                                    System.out.println();
+                                    break;
+                                }
+                            } else {
+                                System.out.println("No se encuentra registro del cliente");
+                                break;
+                            }//Fin if/else
 
-                break;
-            case 2:
-                //SECCION DE ALQUILER DE VEHICULO
-                System.out.println("""
+                        }//fin For
+                    }//Fin if/else
+                    
+                    
+                    break;
+                case 3:
+                    //SECCION DE ALQUILER DE VEHICULO
+                    System.out.println("""
                            Seleccione el tipo de vehiculo que desea alquilar
                            1. Economico
                            2. SUV
                            3. Pickup
                            """);
-                System.out.print("Eleccion: ");
-                respuestaTipoVehiculo = input.nextInt();
-                System.out.println();
+                    System.out.print("Eleccion: ");
+                    respuestaTipoVehiculo = input.nextInt();
+                    System.out.println();
 
-                switch (respuestaTipoVehiculo) {
-                    case 1:
-                        System.out.println("== Tipo de vehiculo seleccionado: ECONOMICO ==");
+                    switch (respuestaTipoVehiculo) {
+                        case 1:
+                            System.out.println("== Tipo de vehiculo seleccionado: ECONOMICO ==");
 
-                        break;
-                    case 2:
-                        System.out.println("== Tipo de vehiculo seleccionado: SUV ==");
+                            break;
+                        case 2:
+                            System.out.println("== Tipo de vehiculo seleccionado: SUV ==");
 
-                        break;
-                    case 3:
-                        System.out.println("== Tipo de vehiculo seleccionado: Pickup ==");
+                            break;
+                        case 3:
+                            System.out.println("== Tipo de vehiculo seleccionado: Pickup ==");
 
-                        break;
-                    default:
-                }//Fin Switch
+                            break;
+                        default:
+                    }//Fin Switch
 
-                break;
-            case 3:
+                    break;
+                case 4:
 
-                break;
-            case 4:
+                    break;
+                default:
 
-                break;
-            default:
+            }//Fin Switch 
 
-        }//Fin Switch        
-  
+        } while (eleccion != 7);
+
     }//Fin Main
-    
+
 }//Fin Class
