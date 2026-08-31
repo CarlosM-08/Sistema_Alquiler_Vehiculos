4882befd355b8500b343f17e7fcc8ac0d625120b
CarlosM-08 <carlos_murillo1@unitec.edu>
2026-08-31 08:07:09 -0600

    Actualización crear función para registro de clientes

--- Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
+++ Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
@@ -34,7 +34,7 @@ public class Proyecto_SistemaAlquilerVehiculos {
         do {
 
             //MENU PRINCIPAL        
-            System.out.println("BIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
+            System.out.println("\nBIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
             System.out.println("-----------------------------------------------");
             System.out.println();
             System.out.println("""
@@ -56,39 +56,15 @@ public class Proyecto_SistemaAlquilerVehiculos {
             switch (eleccion) {
                 case 1:
                     //SECCION REGISTRO DE CLIENTE
-                    System.out.println("== REGISTRO DE CLIENTES==");
-                    System.out.println("");
-                    for (int i = cantidadClientes; i < capacidadMaxClientes; i++) {
-                        System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
-
-                        respuestaRegistro = input.nextLine().toUpperCase();
-                        if (respuestaRegistro.equals("SI")) {
-                            System.out.print("Ingrese el nombre del cliente: ");
-                            cliente[i] = input.nextLine();
-                            System.out.print("Ingrese la edad del cliente: ");
-                            edad[i] = input.nextInt();
-                            System.out.print("Ingrese el numero de identidad del cliente: ");
-                            input.nextLine();
-                            identidad[i] = input.nextLine();
-                            System.out.print("Ingrese el numero de licencia del cliente: ");
-                            licencia[i] = input.nextLine();
-
-                            System.out.println();
-                            System.out.println("== RESUMEN DE REGISTRO ==");
-                            System.out.println("-------------------------");
-                            System.out.println("Nombre del cliente: " + cliente[i]);
-                            System.out.println("edad del cliente: " + edad[i]);
-                            System.out.println("identidad del cliente: " + identidad[i]);
-                            System.out.println("No de licencia del cliente: " + licencia[i]);
-                            System.out.println();
-
-                            cantidadClientes++;
-
-                        } else {
-                            break;
-                        }//Fin if/else
-                    }//Fin for 
-
+                    
+                    cantidadClientes = registrarCliente(input,
+                            cliente,
+                            edad,
+                            identidad,
+                            licencia,
+                            cantidadClientes,
+                            capacidadMaxClientes);                    
+                    
                     break;
                 case 2:
                     //SECCION CONSULTA DE CLIENTE
@@ -159,5 +135,58 @@ public class Proyecto_SistemaAlquilerVehiculos {
         } while (eleccion != 7);
 
     }//Fin Main
+    
+    public static int registrarCliente(
+        Scanner input,
+        String[] cliente,
+        int[] edad,
+        String[] identidad,
+        String[] licencia,
+        int cantidadClientes,
+        int capacidadMaxClientes) {
+
+    String respuestaRegistro = "";
+
+    System.out.println("== REGISTRO DE CLIENTES ==");
+    System.out.println();
+
+    for (int i = cantidadClientes; i < capacidadMaxClientes; i++) {
+
+        System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
+        respuestaRegistro = input.nextLine().toUpperCase();
+
+        if (respuestaRegistro.equals("SI")) {
+
+            System.out.print("Ingrese el nombre del cliente: ");
+            cliente[i] = input.nextLine();
+
+            System.out.print("Ingrese la edad del cliente: ");
+            edad[i] = input.nextInt();
+            input.nextLine();
+
+            System.out.print("Ingrese el numero de identidad del cliente: ");
+            identidad[i] = input.nextLine();
+
+            System.out.print("Ingrese el numero de licencia del cliente: ");
+            licencia[i] = input.nextLine();
+
+            System.out.println();
+            System.out.println("== CLIENTE REGISTRADO, RESUMEN DE REGISTRO ==");
+            System.out.println("---------------------------------------------");
+            System.out.println("Nombre del cliente: " + cliente[i]);
+            System.out.println("Edad del cliente: " + edad[i]);
+            System.out.println("Identidad del cliente: " + identidad[i]);
+            System.out.println("No de licencia del cliente: " + licencia[i]);
+            System.out.println();
+
+            cantidadClientes++;
+
+        } else {
+            break;
+        }//Fin If/Else
+    }//Fin For
+
+    return cantidadClientes;
+}//FIn Funcion registrarCliente
 
 }//Fin Class
