78959bdbec3fbf5366eb01de6ac742ec8121cadf
Cesia Vasquez Avilez <cesia_avilez@unitec.edu>
2026-09-16 00:34:04 -0600

    Avance de proyecto

--- Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
+++ Proyecto_SistemaAlquilerVehiculos/src/proyecto_sistemaalquilervehiculos/Proyecto_SistemaAlquilerVehiculos.java
@@ -10,7 +10,12 @@ import java.util.Scanner;
  *
  * @author cmu08_12mttuz
  */
-public class Proyecto_SistemaAlquilerVehiculos {
+
+
+    /**
+     * @param args the command line arguments
+     */
+ public class Proyecto_SistemaAlquilerVehiculos {
 
     /**
      * @param args the command line arguments
@@ -18,160 +23,358 @@ public class Proyecto_SistemaAlquilerVehiculos {
     public static void main(String[] args) {
         // INICIO DEL PROYECTO
         Scanner input = new Scanner(System.in);
-        
-        //Declaracion de variables
 
+        // Declaracion de variables
         String[] cliente = new String[10];
         int[] edad = new int[10];
         String[] identidad = new String[10];
-        String[] licencia = new String [10];   
-        int respuestaTipoVehiculo = 0;
-        int seleccionVehiculo =0;
+        String[] licencia = new String[10];
+        int respuestaCategoriaVehiculo = 0;
         String respuestaRegistro = "";
         int eleccion = 0;
+        int cantidadClientes = 0;
+        int capacidadMaxClientes = 10;
+        int posicionCliente;
 
-        
-        String[] vehiculo = {"Toyota Corolla","Honda Civic","Toyota Rav4","Ford Explorer","Toyota Hilux"};
-        String[] categoria = {"Economico","Economico","SUV","SUV","Pickup"};
-        double[] tarifa = {30,35,50,60,55};
-        boolean[] disponibles = {true,true,true,true,true};
-        
+        String[] vehiculo = {
+            "Toyota Corolla", "Honda Civic", "Hyundai Elantra",
+            "Kia Rio", "Toyota RAV4", "Ford Explorer",
+            "Honda CR-V", "Hyundai Tucson", "Toyota Hilux",
+            "Ford Ranger", "Nissan Frontier", "Mitsubishi L200"
+        };
 
-        //MENU PRINCIPAL        
-        System.out.println("BIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
-        System.out.println("-----------------------------------------------");
-        System.out.println();
-        System.out.println("""
+        String[] categoria = {
+            "Economico", "Economico", "Economico", "Economico",
+            "SUV", "SUV", "SUV", "SUV",
+            "Pickup", "Pickup", "Pickup", "Pickup"
+        };
+
+        double[] tarifa = {
+            30, 35, 32, 28, 50, 60,
+            52, 48, 55, 58, 53, 50
+        };
+
+        boolean[] disponibles = {
+            true, true, true, true, true, true,
+            true, true, true, true, true, true
+        };
+
+        int seleccionVehiculo = 0;
+        int posicionVehiculo = 0;
+        int dias = 0;
+
+        do {
+
+            // MENU PRINCIPAL
+            System.out.println("\nBIENVENIDOS AL SISTEMA DE ALQUILER DE VEHICULOS");
+            System.out.println("===============================================");
+            System.out.println();
+
+            System.out.println("""
                            Que gestion desea realizar?
                            1. Registrar Cliente
-                           2. Registrar Alquiler
-                           3. Registrar Devolucion
-                           4. Consulta de Vehiculos
-                           5. Mostrar resumen general
-                           6. Salir
+                           2. Consultar Cliente
+                           3. Registrar Alquiler
+                           4. Registrar Devolucion
+                           5. Consulta de Vehiculos
+                           6. Mostrar resumen general
+                           7. Salir
                            """);
-        
-        System.out.print("Eleccion: ");
-        eleccion = input.nextInt();
-        System.out.println();
 
-        switch (eleccion) {
-            case 1:
-                //SECCION REGISTRO DE CLIENTE
-                System.out.println("== REGISTRO DE CLIENTES==");
-                System.out.println("");
+            System.out.print("Eleccion: ");
+            eleccion = input.nextInt();
+            input.nextLine();
+            System.out.println();
 
-                for (int i = 0; i < 10; i++) {
-                    System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
-                    input.nextLine();
-                    respuestaRegistro = input.nextLine().toUpperCase();
-                    if (respuestaRegistro.equals("SI")) {
-                        System.out.print("Ingrese el nombre del cliente: ");
-                        cliente[i] = input.nextLine();
-                        System.out.print("Ingrese la edad del cliente: ");
-                        edad[i] = input.nextInt();
-                     if(edad[i] >= 18) {
-                        System.out.println("El cliente cumple con el requisito de edad.");
-                     } else{
-                         System.out.println("El cliente no cumple con el requisito de edad.");
-                         break;
-                     }
-                        System.out.print("Ingrese el numero de identidad del cliente: ");
-                        input.nextLine();
-                        identidad[i] = input.nextLine();
-                        System.out.print("Ingrese el numero de licencia del cliente: ");
-                        licencia[i] = input.nextLine();
+            switch (eleccion) {
 
-                        System.out.println();                        
-                        System.out.println("== RESUMEN DE REGISTRO ==");
-                        System.out.println("-------------------------");
-                        System.out.println("Nombre del cliente: " + cliente[i]);
-                        System.out.println("edad del cliente: " + edad[i]);
-                        System.out.println("identidad del cliente: " + identidad[i]);
-                        System.out.println("No de licencia del cliente: " + licencia[i]);
-                        System.out.println();
-                    } else {
-                        break;
-                    }//Fin if/else
-                }//Fin for 
+                case 1:
+                    // SECCION REGISTRO DE CLIENTE
 
-                break;
-            case 2:
-                //SECCION DE ALQUILER DE VEHICULO
-                System.out.println("""
-                           Seleccione el tipo de vehiculo que desea alquilar
+                    cantidadClientes = registrarCliente(
+                            input,
+                            cliente,
+                            edad,
+                            identidad,
+                            licencia,
+                            cantidadClientes,
+                            capacidadMaxClientes
+                    );
+
+                    break;
+
+                case 2:
+                    // SECCION CONSULTA DE CLIENTE
+
+                    consultarCliente(
+                            input,
+                            cliente,
+                            edad,
+                            identidad,
+                            licencia,
+                            cantidadClientes
+                    );
+
+                    break;
+
+                case 3:
+                    // SECCION DE ALQUILER DE VEHICULO
+
+                    String categoriaSeleccionada = "No asignada";
+                    double totalPagar = 0;
+                    String metodoPago = "";
+
+                    posicionCliente = buscarCliente(
+                            input,
+                            cliente,
+                            edad,
+                            identidad,
+                            licencia,
+                            cantidadClientes
+                    );
+
+                    if (posicionCliente != -1) {
+
+                        System.out.printf("Bienvenido %s\n", cliente[posicionCliente]);
+
+                        System.out.println("""
+                           Seleccione la categoria de vehiculo que desea alquilar
                            1. Economico
                            2. SUV
                            3. Pickup
                            """);
-                        System.out.print("Eleccion: ");
-                         respuestaTipoVehiculo = input.nextInt();
-                        System.out.println();
 
-                switch (respuestaTipoVehiculo) {
-                    case 1:
-                        System.out.println("== Tipo de vehiculo seleccionado: ECONOMICO ==");
-                        System.out.println();
-                        System.out.println("Vehiculos disponibles");
-                
-                for (int i = 0; i < 5; i++ ){
-                    if (categoria[i].equals("Economico")) {
-                        if(disponibles[i]== true) {
-                    System.out.println((i + 1) + "." + vehiculo[i] + "-$" + tarifa[i]);
-                    
-                  }
-                 }
-                }
-              
-                System.out.print("Seleccione el vehiculo que desea alquilar:");
-                 seleccionVehiculo = input.nextInt();
-                        break;
-                    case 2:
-                        System.out.println("== Tipo de vehiculo seleccionado: SUV ==");
-                for (int i = 0; i < 5; i++ ){
-                    if (categoria[i].equals("SUV")) {
-                        if(disponibles[i]== true) {
-                    System.out.println((i + 1) + "." + vehiculo[i] + "-$" + tarifa[i]);
-                    
-                  }
-                 }
-                }
-                       
-                System.out.println("Seleccione el vehiculo que deasea alquilar:");
-                 seleccionVehiculo = input.nextInt();
-                        break;
-                case 3:
-                         System.out.println("== Tipo de vehiculo seleccionado: Pickup ==");
-                       for (int i = 0; i < 5; i++ ){
-                    if (categoria[i].equals("Pickup")) {
-                        if(disponibles[i]== true) {
-                    System.out.println((i + 1) + "." + vehiculo[i] + "-$" + tarifa[i]);
-                    
-                  }
-                 }
-                }
-                
-                System.out.println("Seleccione el vehiculo que deasea alquilar:");
-                 seleccionVehiculo = input.nextInt();
-                        break;
-                    default:
-                }//Fin Switch
-                
-                
-                break;
-            case 3:
+                        do {
+                            System.out.print("Eleccion: ");
+                            respuestaCategoriaVehiculo = input.nextInt();
+                            System.out.println();
 
-                break;
-            case 4:
+                            if (respuestaCategoriaVehiculo < 1
+                                    || respuestaCategoriaVehiculo > 3) {
+                                System.out.println("Eleccion no valida!!");
+                            }
 
-                break;
-            default:
+                        } while (respuestaCategoriaVehiculo < 1
+                                || respuestaCategoriaVehiculo > 3);
 
-        }//Fin Switch        
-  
-    }//Fin Main
+                        switch (respuestaCategoriaVehiculo) {
 
-    
+                            case 1:
+                                categoriaSeleccionada = "Economico";
+                                break;
+
+                            case 2:
+                                categoriaSeleccionada = "SUV";
+                                break;
+
+                            case 3:
+                                categoriaSeleccionada = "Pickup";
+                                break;
+
+                            default:
+                                System.out.println("Opcion no valida");
+                        }
+
+                        for (int i = 0; i < categoria.length; i++) {
+
+                            if (categoria[i].equals(categoriaSeleccionada)
+                                    && disponibles[i] == true) {
+
+                                System.out.println(
+                                        (i + 1) + ") "
+                                        + vehiculo[i] + " - "
+                                        + tarifa[i]
+                                );
+                            }
+                        }
+
+                        System.out.print("\nSeleccione el vehiculo que desea alquilar: ");
+                        seleccionVehiculo = input.nextInt();
+
+                        posicionVehiculo = seleccionVehiculo - 1;
+
+                        if (posicionVehiculo >= 0
+                                && posicionVehiculo <= vehiculo.length - 1) {
+
+                            if (categoria[posicionVehiculo].equals(categoriaSeleccionada)
+                                    && disponibles[posicionVehiculo] == true) {
+
+                                System.out.println(vehiculo[posicionVehiculo]);
+                                System.out.println(categoria[posicionVehiculo]);
+                                System.out.println(tarifa[posicionVehiculo]);
+
+                                do {
+                                    System.out.print("Cuantos dias desea alquilar el vehiculo: ");
+                                    dias = input.nextInt();
+
+                                    if (dias <= 0) {
+                                        System.out.println("Cantidad de dias no valida");
+                                    }
+
+                                } while (dias <= 0);
+
+                                input.nextLine();
+
+                                System.out.print("Ingrese el metodo de pago: ");
+                                metodoPago = input.nextLine();
+
+                                totalPagar = tarifa[posicionVehiculo] * dias;
+
+                                disponibles[posicionVehiculo] = false;
+
+                                System.out.println("\n\nResumen del alquiler");
+                                System.out.println("=================================");
+                                System.out.printf("Cliente: %s\n", cliente[posicionCliente]);
+                                System.out.printf("Vehiculo seleccionado: %s\n", vehiculo[posicionVehiculo]);
+                                System.out.printf("Cantidad de dias: %d\n", dias);
+                                System.out.printf("Metodo de pago: %s\n", metodoPago);
+                                System.out.printf("Total a pagar: %.2f\n", totalPagar);
+                                System.out.println("=================================\n\n");
+
+                            } else {
+                                System.out.println(
+                                        "El vehículo seleccionado no está disponible "
+                                        + "o no pertenece a la categoría seleccionada"
+                                );
+                            }
+
+                        } else {
+                            System.out.println("Selección de vehículo no válida");
+                        }
+
+                    }
+            }
+
+        } while (eleccion != 7);
+
     }
+
+    public static int registrarCliente(
+            Scanner input,
+            String[] cliente,
+            int[] edad,
+            String[] identidad,
+            String[] licencia,
+            int cantidadClientes,
+            int capacidadMaxClientes) {
+
+        String respuestaRegistro = "";
+
+        System.out.println("== REGISTRO DE CLIENTES ==");
+        System.out.println();
+
+        for (int i = cantidadClientes; i < capacidadMaxClientes; i++) {
+
+            System.out.print("Desea registrar un nuevo cliente? (SI/NO): ");
+            respuestaRegistro = input.nextLine().toUpperCase();
+
+            if (respuestaRegistro.equals("SI")) {
+
+                System.out.print("Ingrese el nombre del cliente: ");
+                cliente[i] = input.nextLine();
+
+                System.out.print("Ingrese la edad del cliente: ");
+                edad[i] = input.nextInt();
+                input.nextLine();
+
+                System.out.print("Ingrese el numero de identidad del cliente: ");
+                identidad[i] = input.nextLine();
+
+                System.out.print("Ingrese el numero de licencia del cliente: ");
+                licencia[i] = input.nextLine();
+
+                System.out.println();
+                System.out.println("CLIENTE REGISTRADO!, RESUMEN DE REGISTRO");
+                System.out.println("========================================");
+                System.out.println("Nombre del cliente: " + cliente[i]);
+                System.out.println("Edad del cliente: " + edad[i]);
+                System.out.println("Identidad del cliente: " + identidad[i]);
+                System.out.println("No de licencia del cliente: " + licencia[i]);
+                System.out.println();
+
+                cantidadClientes++;
+
+            } else {
+                break;
+            }
+
+        }
+
+        return cantidadClientes;
+
+    }
+        public static void consultarCliente(
+            Scanner input,
+            String[] cliente,
+            int[] edad,
+            String[] identidad,
+            String[] licencia,
+            int cantidadClientes) {
+
+        System.out.println("== CONSULTA DE CLIENTES ==");
+        System.out.println("");
+
+        int posicion;
+
+        posicion = buscarCliente(
+                input,
+                cliente,
+                edad,
+                identidad,
+                licencia,
+                cantidadClientes
+        );
+
+        if (posicion != -1) {
+
+            System.out.println("Nombre del cliente: " + cliente[posicion]);
+            System.out.println("edad del cliente: " + edad[posicion]);
+            System.out.println("identidad del cliente: " + identidad[posicion]);
+            System.out.println("licencia del cliente: " + licencia[posicion]);
+            System.out.println();
+        }
+    }
+        public static int buscarCliente(
+            Scanner input,
+            String[] cliente,
+            int[] edad,
+            String[] identidad,
+            String[] licencia,
+            int cantidadClientes) {
+
+        String valorBuscado;
+        boolean valorEncontrado = false;
+        int posicionClienteTem = -1;
+
+        if (cantidadClientes == 0) {
+
+            System.out.println("=================================");
+            System.out.println("|| NO hay clientes registrados ||");
+            System.out.println("=================================");
+
+        } else {
+
+            System.out.print("Ingrese el numero de identidad del cliente: ");
+            valorBuscado = input.nextLine();
+            for (int i = 0; i < cantidadClientes; i++) {
+
+                if (valorBuscado.equals(identidad[i])) {
+
+                    valorEncontrado = true;
+                    posicionClienteTem = i;
+                    break;
+                }
+            }
+                if (valorEncontrado == false) {
+
+                System.out.println("==========================================");
+                System.out.println("|| No se encuentra registro del cliente ||");
+                System.out.println("==========================================");
+            }
+        }
+
+       return posicionClienteTem;
+    }
+}
     
 
